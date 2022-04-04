e204aae5f1c8b32902fdf60fbabdd93f7dbb1f4a
Fede Rivero <federicorivero18@gmail.com>
2022-04-03 22:02:48 -0300

    first commit and deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import "./App.css";
+import axios from "axios";
+import TableCoins from "./components/TableCoins";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getData = async () => {
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"
+    );
+    // console.log(res.data);
+    setCoins(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="bg-dark text-secondary px-4 py-5 text-center">
+        <div className="py-2">
+          <div className="col-lg-6 mx-auto">
+            
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <figure className="img-fluid px-4 me-sm-3">
+                <Player
+                  autoplay
+                  loop
+                  background={"transparent"}
+                  src="https://assets1.lottiefiles.com/private_files/lf30_h03e9pog.json"
+                  style={{ height: "150px", width: "150px" }}
+                ></Player>
+              </figure>
+
+              <h1 className="display-5 fw-bold text-white px-4 mt-4">
+                Market Coin
+              </h1>
+              <hr />
+            </div>
+            <p className="fs-5">
+              A simple website using the coingecko API to display the
+              cryptocurrency prices.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <input
+        type="text"
+        placeholder="Search price coin"
+        className="form-control bg-dark text-light fs-5 border-0 mt-2 mb-1 text-center"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <TableCoins coins={coins} search={search} />
     </div>
+
   );
 }
 
---
+++ src/components/TableCoins.js
@@ -0,0 +1,50 @@
+import React from "react";
+import CoinRow from "./CoinRow";
+
+const titles = ["#", "Coin", "Price", "Price Change", "24h Volume"];
+
+const TableCoins = ({ coins, search }) => {
+  const filteredCoins = coins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase()) |
+      coin.symbol.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="container-fluid">
+      <div className="table-responsive">
+        <table className="table table-dark mt-4 table-hover">
+          <thead>
+            <tr>
+              {titles.map((title, index) => (
+                <td key={index}>{title}</td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {filteredCoins.map((coin, index) => (
+              <CoinRow key={index} coin={coin} index={index + 1} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    // <table classNameName="table table-dark mt-4 table-hover">
+    //   <thead>
+    //     <tr>
+    //       {titles.map((title, index) => (
+    //         <td key={index}>{title}</td>
+    //       ))}
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {filteredCoins.map((coin, index) => (
+    //       <CoinRow key={index} coin={coin} index={index + 1} />
+    //     ))}
+    //   </tbody>
+    // </table>
+  );
+};
+
+export default TableCoins;
